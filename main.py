868a2b9abcc9fc7901210005b87c1cb289016485
Juandiego <juanoriundo2017@gmail.com>
2026-08-27 18:59:20 -0500

    Subiendo backend y modelo para Render

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from ultralytics import YOLO
+from PIL import Image
+import io
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = YOLO("best.pt")
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+    
+    results = model(image)
+    
+    # Obtenemos las probabilidades de todas las clases
+    probs = results[0].probs.data.tolist()
+    names = results[0].names
+    
+    # Mapeamos cada clase con su porcentaje
+    resultados_clases = {names[i]: round(float(probs[i]) * 100, 2) for i in range(len(probs))}
+    
+    # Determinamos la principal
+    top1_index = results[0].probs.top1
+    class_name = names[top1_index]
+    confidence = results[0].probs.top1conf.item() * 100
+
+    return {
+        "clase": class_name,
+        "precision": round(confidence, 2),
+        "detalles": resultados_clases
+    }
